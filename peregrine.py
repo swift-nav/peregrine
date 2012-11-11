917d1930f9f4eb2905cfda23e515ab621904d8cd
Fergus Noble <fergusnoble@gmail.com>
2012-11-11 19:10:14 +0700

    Tidying up. Updating file header comment.

--- peregrine.py
+++ peregrine.py
@@ -1,52 +1,54 @@
 #!/usr/bin/env python
-#--------------------------------------------------------------------------
-#                           SoftGNSS v3.0
+
+# Copyright (C) 2012 Swift Navigation Inc.
 #
-# Copyright (C) Darius Plausinaitis and Dennis M. Akos
-# Written by Darius Plausinaitis and Dennis M. Akos
-# Converted to Python by Colin Beighley
-#--------------------------------------------------------------------------
-#This program is free software; you can redistribute it and/or
-#modify it under the terms of the GNU General Public License
-#as published by the Free Software Foundation; either version 2
-#of the License, or (at your option) any later version.
+# This source is subject to the license found in the file 'LICENSE' which must
+# be be distributed together with this source. All other rights reserved.
 #
-#This program is distributed in the hope that it will be useful,
-#but WITHOUT ANY WARRANTY; without even the implied warranty of
-#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#GNU General Public License for more details.
-#
-#You should have received a copy of the GNU General Public License
-#along with this program; if not, write to the Free Software
-#Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
-#USA.
-#--------------------------------------------------------------------------
+# THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND,
+# EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
 
 import sys
 import argparse
-from initSettings import initSettings
+import pickle
+import logging
+from operator import attrgetter
+
 import peregrine.getSamples as getSamples
 from peregrine.acquisition import acquisition
 from peregrine.navigation import navigation
-import pickle
 from peregrine.include.showChannelStatus import showChannelStatus
 from peregrine.tracking import track
-import logging
-from operator import attrgetter
-logging.basicConfig(level=logging.DEBUG, format="%(asctime)s [%(levelname)s] %(name)s: %(message)s")
 
-#Initialize constants, settings
+from initSettings import initSettings
+
+# Configure logging
+logging.basicConfig(
+  level=logging.DEBUG,
+  format="%(asctime)s [%(levelname)s] %(name)s: %(message)s"
+)
+
+# Initialize constants, settings
 settings = initSettings()
 
 parser = argparse.ArgumentParser()
-parser.add_argument("file", help="the sample data file to process")
-parser.add_argument("-a", "--skip-acquisition", help="use previously saved acquisition results", action="store_true")
-parser.add_argument("-t", "--skip-tracking", help="use previously saved tracking results", action="store_true")
-parser.add_argument("-n", "--skip-navigation", help="use previously saved navigation results", action="store_true")
+parser.add_argument("file",
+                    help="the sample data file to process")
+parser.add_argument("-a", "--skip-acquisition",
+                    help="use previously saved acquisition results",
+                    action="store_true")
+parser.add_argument("-t", "--skip-tracking",
+                    help="use previously saved tracking results",
+                    action="store_true")
+parser.add_argument("-n", "--skip-navigation",
+                    help="use previously saved navigation results",
+                    action="store_true")
 args = parser.parse_args()
 settings.fileName = args.file
 
-samplesPerCode = int(round(settings.samplingFreq / (settings.codeFreqBasis / settings.codeLength)))
+samplesPerCode = int(round(settings.samplingFreq /
+                           (settings.codeFreqBasis / settings.codeLength)))
 
 # Do acquisition
 acq_results_file = args.file + ".acq_results"
@@ -56,17 +58,20 @@ if args.skip_acquisition:
     with open(acq_results_file, 'rb') as f:
       acq_results = pickle.load(f)
   except IOError:
-    logging.critical("Couldn't open acquisition results file '%s'.", acq_results_file)
+    logging.critical("Couldn't open acquisition results file '%s'.",
+                     acq_results_file)
     sys.exit(1)
 else:
   # Get 11ms of acquisition samples for fine frequency estimation
-  acq_samples = getSamples.int8(args.file, 11*samplesPerCode, settings.skipNumberOfBytes)
+  acq_samples = getSamples.int8(args.file, 11*samplesPerCode,
+                                settings.skipNumberOfBytes)
   acq_results = acquisition(acq_samples, settings)
   try:
     with open(acq_results_file, 'wb') as f:
       pickle.dump(acq_results, f)
   except IOError:
-    logging.error("Couldn't save acquisition results file '%s'.", acq_results_file)
+    logging.error("Couldn't save acquisition results file '%s'.",
+                  acq_results_file)
 
 if len(acq_results) == 0:
   logging.critical("No satellites acquired!")
@@ -84,7 +89,8 @@ if args.skip_tracking:
     with open(track_results_file, 'rb') as f:
       (track_results, channel) = pickle.load(f)
   except IOError:
-    logging.critical("Couldn't open tracking results file '%s'.", track_results_file)
+    logging.critical("Couldn't open tracking results file '%s'.",
+                     track_results_file)
     sys.exit(1)
 else:
   (track_results, channel) = track(acq_results, settings)
@@ -92,7 +98,8 @@ else:
     with open(track_results_file, 'wb') as f:
       pickle.dump((track_results, channel), f)
   except IOError:
-    logging.error("Couldn't save tracking results file '%s'.", track_results_file)
+    logging.error("Couldn't save tracking results file '%s'.",
+                  track_results_file)
 
 # Do navigation
 if not args.skip_navigation:
