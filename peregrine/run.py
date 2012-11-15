0f15b0965d04ccc6c04d16ce2867281c3de108a2
Fergus Noble <fergusnoble@gmail.com>
2012-11-15 20:37:16 +0700

    Adding setup.py script for installation, adding install documentation.
    Also:
     * bootstraps Distribute.
     * Uses entry_points mechanism for main script.

---
+++ peregrine/run.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python
+
+# Copyright (C) 2012 Swift Navigation Inc.
+#
+# This source is subject to the license found in the file 'LICENSE' which must
+# be be distributed together with this source. All other rights reserved.
+#
+# THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND,
+# EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
+
+import sys
+import argparse
+import pickle
+import logging
+from operator import attrgetter
+
+import peregrine.get_samples as get_samples
+from peregrine.acquisition import Acquisition
+from peregrine.navigation import navigation
+from peregrine.tracking import track
+
+from initSettings import initSettings
+
+def main():
+  # Configure logging
+  # Note: An ANSI escape sequence is used to clear the line in case a
+  # progressbar is running but this won't work in Windows.
+  logging.basicConfig(
+    level=logging.DEBUG,
+    format="\033[2K\r%(asctime)s [%(levelname)s] %(name)s: %(message)s"
+  )
+
+  # Initialize constants, settings
+  settings = initSettings()
+
+  parser = argparse.ArgumentParser()
+  parser.add_argument("file",
+                      help="the sample data file to process")
+  parser.add_argument("-a", "--skip-acquisition",
+                      help="use previously saved acquisition results",
+                      action="store_true")
+  parser.add_argument("-t", "--skip-tracking",
+                      help="use previously saved tracking results",
+                      action="store_true")
+  parser.add_argument("-n", "--skip-navigation",
+                      help="use previously saved navigation results",
+                      action="store_true")
+  args = parser.parse_args()
+  settings.fileName = args.file
+
+  samplesPerCode = int(round(settings.samplingFreq /
+                             (settings.codeFreqBasis / settings.codeLength)))
+
+  # Do acquisition
+  acq_results_file = args.file + ".acq_results"
+  if args.skip_acquisition:
+    logging.info("Skipping acquisition, loading saved acquisition results.")
+    try:
+      with open(acq_results_file, 'rb') as f:
+        acq_results = pickle.load(f)
+    except IOError:
+      logging.critical("Couldn't open acquisition results file '%s'.",
+                       acq_results_file)
+      sys.exit(1)
+  else:
+    # Get 11ms of acquisition samples for fine frequency estimation
+    acq_samples = get_samples.int8(args.file, 11*samplesPerCode,
+                                   settings.skipNumberOfBytes)
+    acq = Acquisition(acq_samples, settings.samplingFreq, settings.IF, samplesPerCode)
+    acq_results = acq.acquisition()
+    try:
+      with open(acq_results_file, 'wb') as f:
+        pickle.dump(acq_results, f)
+      logging.debug("Saving acquisition results as '%s'" % acq_results_file)
+    except IOError:
+      logging.error("Couldn't save acquisition results file '%s'.",
+                    acq_results_file)
+
+  if len(acq_results) == 0:
+    logging.critical("No satellites acquired!")
+    sys.exit(1)
+
+  acq_results.sort(key=attrgetter('snr'), reverse=True)
+
+  # Track the acquired satellites
+  track_results_file = args.file + ".track_results"
+  if args.skip_tracking:
+    logging.info("Skipping tracking, loading saved tracking results.")
+    try:
+      with open(track_results_file, 'rb') as f:
+        (track_results, channel) = pickle.load(f)
+    except IOError:
+      logging.critical("Couldn't open tracking results file '%s'.",
+                       track_results_file)
+      sys.exit(1)
+  else:
+    (track_results, channel) = track(acq_results, settings)
+    try:
+      with open(track_results_file, 'wb') as f:
+        pickle.dump((track_results, channel), f)
+      logging.debug("Saving tracking results as '%s'" % track_results_file)
+    except IOError:
+      logging.error("Couldn't save tracking results file '%s'.",
+                    track_results_file)
+
+  # Do navigation
+  if not args.skip_navigation:
+    navSolutions = navigation(track_results, settings)
+
+if __name__ == '__main__':
+  main()
---
+++ setup.py
@@ -0,0 +1,139 @@
+#!/usr/bin/env python
+
+"""
+============
+Installation
+============
+
+Requirements
+============
+
+Peregrine has the following requirements:
+
+- `Python <http://www.python.org/>`_ 2.6 or 2.7
+
+- `Numpy <http://www.numpy.org/>`_ 1.6 or later
+
+- `pyFFTW <http://pypi.python.org/pypi/pyFFTW>`_ 0.8.2 or later
+
+- `libswiftnav <https://github.com/swift-nav/Swift-Nav-Code>`_ and its Python
+  bindings
+
+Peregrine also depends on other projects for optional features.
+
+- `progressbar <http://code.google.com/p/python-progressbar/>`_ to display a
+  progress indication in the terminal whilst running.
+
+- `matplotlib <http://matplotlib.org/>`_ to support generation of plots.
+
+Installing Peregrine
+====================
+
+Obtaining the source
+--------------------
+
+The latest development version of Peregrine can be cloned from github
+using this command::
+
+   git clone git://github.com/swift-nav/peregrine.git
+
+Building and Installing
+-----------------------
+
+.. note::
+  `pyFFTW <http://pypi.python.org/pypi/pyFFTW>`_ requires `FFTW3
+  <http://www.fftw.org/>`_ which should be available through your operating
+  system package manager. For Mac OS X we recommend using `Homebrew
+  <http://mxcl.github.com/homebrew/>`_.
+
+.. note::
+  Installation requires `Distribute <http://pypi.python.org/pypi/distribute>`_,
+  if your python installation doesn't provide this it will automatically be
+  installed.
+
+To install Peregrine (from the root of the source tree)::
+
+    $ python setup.py install
+
+This will download and install any required python modules from `PyPI
+<http://pypi.python.org/>`_.
+
+If you are a developer and intend to make modifications to Peregrine then you
+can instead run::
+
+    $ python setup.py develop
+
+Which is similar to `install` but instead of copying Peregrine to the install
+location, a link is made from the source location to the install location so
+you can continue to work on the soruce without having to run `install` every
+time you wish to test your changes.
+
+Installing optional dependencies
+--------------------------------
+
+The optional dependencies can be installed with `pip` or `easy_install` as
+follows::
+
+  $ pip install progressbar matplotlib
+
+Building documentation
+----------------------
+
+.. note::
+    Building the documentation is in general not necessary unless you are
+    writing new documentation or do not have internet access, because the
+    latest (and archive) versions of Peregrine's documentation should be
+    available at `docs.swift-nav.com <http://docs.swift-nav.com>`_ .
+
+Building the documentation requires the Peregrine source code and some
+additional packages:
+
+    - `Sphinx <http://sphinx.pocoo.org>`_ (and its dependencies) 1.0 or later
+
+To build the Peregrine documentation, execute the following commands::
+
+    $ cd docs
+    $ make html
+
+The documentation will be built in the ``docs/_build/html`` directory, and can
+be read by pointing a web browser to ``docs/_build/html/index.html``.
+
+"""
+
+setup_args = dict(
+  name = 'Peregrine',
+  version = '0.1dev',
+  description = 'Peregrine software GNSS receiver',
+  license = "GPLv3",
+  url = 'http://www.swift-nav.com',
+
+  author = 'Swift Navigation Inc.',
+  maintainer = 'Fergus Noble',
+  maintainer_email = 'fergus@swift-nav.com',
+
+  packages = ['peregrine'],
+
+  entry_points = {
+    'console_scripts':
+      ['peregrine = peregrine.run:main']
+  },
+
+  install_requires = [
+    'swiftnav',
+    'pyFFTW >= 0.8.2',
+    'numpy >= 1.6',
+  ],
+  extras_require = {
+    'progress': ['progressbar >= 2.3'],
+    'plot': ['matplotlib >= 1.1'],
+  }
+)
+
+if __name__ == "__main__":
+  # Bootstrap Distribute if the user doesn't have it
+  from distribute_setup import use_setuptools
+  use_setuptools()
+
+  from setuptools import setup
+
+  setup(**setup_args)
