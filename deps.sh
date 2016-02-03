0043399c380b74d574cb334a15fe857548f68a2b
Henry Hallam <henry@swiftnav.com>
2016-02-02 23:49:11 -0800

    Fix deps on Debian

--- deps.sh
+++ deps.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-function install_deps_debian () {
+function install_deps_ubuntu_maybe () {
     # Sudo'd version of travis installation instructions
     sudo apt-get update -qq
     sudo apt-get install python-software-properties
@@ -34,6 +34,37 @@ function install_deps_debian () {
     sudo python setup.py develop
 }
 
+function install_deps_debian_jessie_or_stretch () {
+    # Sudo'd version of travis installation instructions
+    sudo apt-get update -qq
+    sudo apt-get -y install cmake \
+         check \
+         fftw3 \
+         libfftw3-dev \
+         python-pip \
+         build-essential \
+         python-numpy \
+         python-dev \
+         cython \
+         python-dev
+    # Build and install libswiftnav
+    git submodule update --init
+    cd libswiftnav/
+    mkdir -p build && cd build/
+    cmake ../
+    make -j$(nproc)
+    sudo make install
+    sudo ldconfig
+    # Don't think we need to build/install the libswiftnav python
+    # bindings; the peregrine pip install will do that for us.
+#    cd ../python
+#    python setup.py build
+#    sudo python setup.py install
+    cd ../../
+    sudo pip install -r requirements.txt
+    sudo python setup.py develop
+}
+
 function install_deps_osx () {
     # TODO: Add OS X brew installation dependencies
     if [[ ! -x /usr/local/bin/brew ]]; then
@@ -57,7 +88,11 @@ function install_deps_osx () {
 }
 
 if [[ "$OSTYPE" == "linux-"* ]]; then
-    install_deps_debian
+    if egrep -q "Debian GNU/Linux (jessie|stretch)" /etc/issue; then
+        install_deps_debian_jessie_or_stretch
+    else
+        install_deps_ubuntu_maybe
+    fi
 elif [[ "$OSTYPE" == "darwin"* ]]; then
     install_deps_osx
 else
