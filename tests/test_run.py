3fc6e1de8e3f5ce56d9c956d6e524353f705c109
Colin Beighley <colinbeighley@gmail.com>
2016-02-12 11:41:30 -0800

    Add tracking test

--- tests/test_run.py
+++ tests/test_run.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2015 Swift Navigation Inc.
+# Copyright (C) 2016 Swift Navigation Inc.
 #
 # This source is subject to the license found in the file 'LICENSE' which must
 # be be distributed together with this source. All other rights reserved.
@@ -32,29 +32,49 @@ NEW_NAV_RES = SAMPLES_PATH + SAMPLES_FNAME + '.nav_results'
 
 def test_acquisition():
 
+  # Replace argv with args to skip tracking and navigation.
   with patch.object(sys, 'argv', ['peregrine', SAMPLES, '-t', '-n']):
 
     try:
       peregrine.run.main()
     except SystemExit:
-      # Will be thrown if track or nav results files are not present
+      # Thrown if track and nav results files are not present and we
+      # supplied command line args to skip tracking and navigation.
       pass
     
-    # Load acquisition results from test run
     new_acq_results = load_acq_results(NEW_ACQ_RES)
-
-    # Load saved acq results to compare against
     old_acq_results = load_acq_results(OLD_ACQ_RES)
 
     assert new_acq_results == old_acq_results
 
+    # Clean-up.
     os.remove(NEW_ACQ_RES)
 
-#def test_tracking():
-#  with patch.object(sys, 'argv', ['peregrine', SAMPLES, '-a', '-n']):
-#    peregrine.run.main()
-#    os.remove(ACQ_RES)
-#    os.remove(TRK_RES)
+def test_tracking():
+
+  # Replace argv with args to skip acquisition and navigation.
+  with patch.object(sys, 'argv', ['peregrine', SAMPLES, '-a', '-n']):
+
+    # Copy reference acq results to use to skip acquisition.
+    os.system('cp ' + OLD_ACQ_RES + ' ' + NEW_ACQ_RES)
+
+    try:
+      peregrine.run.main()
+    except SystemExit:
+      # Thrown if nav results file is not present and we supplied
+      # command line arg to skip navigation.
+      pass
+
+    with open(NEW_TRK_RES, 'rb') as f:
+      new_trk_results = cPickle.load(f)
+    with open(OLD_TRK_RES, 'rb') as f:
+      old_trk_results = cPickle.load(f)
+
+    assert new_trk_results == old_trk_results
+
+    # Clean-up.
+    os.remove(NEW_ACQ_RES)
+    os.remove(NEW_TRK_RES)
 
 # TODO
 #def test_navigation():
