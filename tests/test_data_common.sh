8a28a9a0a0edd4d78a2ad7f8836af47b07373da6
Colin Beighley <colinbeighley@gmail.com>
2016-02-18 14:49:02 -0800

    Correct S3 bucket

--- tests/test_data_common.sh
+++ tests/test_data_common.sh
@@ -2,7 +2,7 @@
 
 export TEST_DATA_TAR=peregrine_ci_test_data.tar.gz
 export TEST_DATA_SHA=53b0fe1978ae7761e81cbbfc468ccd8758deebd5
-export TEST_DATA_URL_PATH=downloads/baseband_samples
+export TEST_DATA_URL_PATH=downloads.swiftnav.com/baseband_samples
 export TEST_DATA_URL=http://downloads.swiftnav.com.s3-us-west-1.amazonaws.com/baseband_samples/$TEST_DATA_TAR
 
 # Remove filename from shasum
