a67db081f4b1114cbbb948a697cdb0d8c88921ae
Colin Beighley <colinbeighley@gmail.com>
2016-02-22 11:44:06 -0800

    Delete old tarball

--- tests/get_test_data.sh
+++ tests/get_test_data.sh
@@ -6,6 +6,7 @@ source test_data_common.sh
 
 # Archive old data, fail silently if it doesn't exist
 mv test_data test_data_old_$(date +%Y-%m-%d:%H:%M:%S) 2>/dev/null || true
+rm -f $TEST_DATA_TAR
 
 curl -O $TEST_DATA_URL
 
