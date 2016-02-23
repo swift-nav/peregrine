6709580567a56c5200aa90f8a5c417e8106f1401
Colin Beighley <colinbeighley@gmail.com>
2016-02-22 18:33:23 -0800

    Update tarball SHA

--- tests/test_data_common.sh
+++ tests/test_data_common.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 export TEST_DATA_TAR=peregrine_ci_test_data.tar.gz
-export TEST_DATA_SHA=260f5e3bc5d3c581b03a955547ad02c87d82efcd
+export TEST_DATA_SHA=f9580a1a1589f381077e8247236e8ae64cf5043a
 export TEST_DATA_URL_PATH=downloads.swiftnav.com/baseband_samples/$TEST_DATA_TAR
 export TEST_DATA_URL=http://downloads.swiftnav.com.s3-us-west-1.amazonaws.com/baseband_samples/$TEST_DATA_TAR
 
