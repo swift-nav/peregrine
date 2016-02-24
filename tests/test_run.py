93d68be788588e155386717e331ff2f784211ee7
Colin Beighley <colinbeighley@gmail.com>
2016-02-23 16:52:59 -0800

    Add tests back into test_run.py

--- tests/test_run.py
+++ tests/test_run.py
@@ -34,51 +34,51 @@ NEW_ACQ_RES = SAMPLES_PATH + SAMPLES_FNAME + '.acq_results'
 NEW_TRK_RES = SAMPLES_PATH + SAMPLES_FNAME + '.track_results'
 NEW_NAV_RES = SAMPLES_PATH + SAMPLES_FNAME + '.nav_results'
 
-#def test_acquisition():
-#
-#  # Replace argv with args to skip tracking and navigation.
-#  with patch.object(sys, 'argv', ['peregrine', SAMPLES, '-t', '-n']):
-#
-#    try:
-#      peregrine.run.main()
-#    except SystemExit:
-#      # Thrown if track and nav results files are not present and we
-#      # supplied command line args to skip tracking and navigation.
-#      pass
-#    
-#    new_acq_results = load_acq_results(NEW_ACQ_RES)
-#    old_acq_results = load_acq_results(OLD_ACQ_RES)
-#
-#    assert new_acq_results == old_acq_results
-#
-#    # Clean-up.
-#    os.remove(NEW_ACQ_RES)
+def test_acquisition():
 
-#def test_tracking():
-#
-#  # Replace argv with args to skip acquisition and navigation.
-#  with patch.object(sys, 'argv', ['peregrine', SAMPLES, '-a', '-n']):
-#
-#    # Copy reference acq results to use in order to skip acquisition.
-#    copyfile(OLD_ACQ_RES, NEW_ACQ_RES)
-#
-#    try:
-#      peregrine.run.main()
-#    except SystemExit:
-#      # Thrown if nav results file is not present and we supplied
-#      # command line arg to skip navigation.
-#      pass
-#
-#    with open(NEW_TRK_RES, 'rb') as f:
-#      new_trk_results = cPickle.load(f)
-#    with open(OLD_TRK_RES, 'rb') as f:
-#      old_trk_results = cPickle.load(f)
-#
-#    assert new_trk_results == old_trk_results
-#
-#    # Clean-up.
-#    os.remove(NEW_ACQ_RES)
-#    os.remove(NEW_TRK_RES)
+  # Replace argv with args to skip tracking and navigation.
+  with patch.object(sys, 'argv', ['peregrine', SAMPLES, '-t', '-n']):
+
+    try:
+      peregrine.run.main()
+    except SystemExit:
+      # Thrown if track and nav results files are not present and we
+      # supplied command line args to skip tracking and navigation.
+      pass
+    
+    new_acq_results = load_acq_results(NEW_ACQ_RES)
+    old_acq_results = load_acq_results(OLD_ACQ_RES)
+
+    assert new_acq_results == old_acq_results
+
+    # Clean-up.
+    os.remove(NEW_ACQ_RES)
+
+def test_tracking():
+
+  # Replace argv with args to skip acquisition and navigation.
+  with patch.object(sys, 'argv', ['peregrine', SAMPLES, '-a', '-n']):
+
+    # Copy reference acq results to use in order to skip acquisition.
+    copyfile(OLD_ACQ_RES, NEW_ACQ_RES)
+
+    try:
+      peregrine.run.main()
+    except SystemExit:
+      # Thrown if nav results file is not present and we supplied
+      # command line arg to skip navigation.
+      pass
+
+    with open(NEW_TRK_RES, 'rb') as f:
+      new_trk_results = cPickle.load(f)
+    with open(OLD_TRK_RES, 'rb') as f:
+      old_trk_results = cPickle.load(f)
+
+    assert new_trk_results == old_trk_results
+
+    # Clean-up.
+    os.remove(NEW_ACQ_RES)
+    os.remove(NEW_TRK_RES)
 
 def test_blah():
 
