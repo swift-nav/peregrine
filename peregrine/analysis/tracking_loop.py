4f36bb6b6737a224fd1f5fd6d08da4e39aef80f9
Adel Mamin <adelm@exafore.com>
2016-02-26 17:09:40 +0200

    Make 'custom_rate' and 'peregrine' frequency profiles synonims

--- peregrine/analysis/tracking_loop.py
+++ peregrine/analysis/tracking_loop.py
@@ -84,8 +84,8 @@ def main():
 
   parser.add_argument("--profile",
                       help="L1C/A & L2C IF + sampling frequency profile"
-                      "('peregrine', 'low_rate', 'normal_rate' (piksi_v3)"
-                      "'high_rate')",
+                      "('peregrine'/'custom_rate', 'low_rate', "
+                      "'normal_rate' (piksi_v3), 'high_rate')",
                       default = 'peregrine')
 
   parser.add_argument("-P", "--prn",
@@ -107,7 +107,7 @@ def main():
 
   args = parser.parse_args()
 
-  if args.profile == 'peregrine':
+  if args.profile == 'peregrine' or args.profile == 'custom_rate':
     freq_profile = defaults.freq_profile_peregrine
   elif args.profile == 'low_rate':
     freq_profile = defaults.freq_profile_low_rate
