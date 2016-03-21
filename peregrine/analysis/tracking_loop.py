689f6ccf63b1514b9afe4dda4a912f609424c9ea
Valeri Atamaniouk <valeri@swift-nav.com>
2016-03-21 09:45:59 +0200

    peregrine: added support for short/long cycles
    Added support for simulating short/long cycles firmware behaviour.

--- peregrine/analysis/tracking_loop.py
+++ peregrine/analysis/tracking_loop.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 # Copyright (C) 2016 Swift Navigation Inc.
 # Contact: Adel Mamin <adelm@exafore.com>
 #
@@ -8,77 +10,132 @@
 # EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED
 # WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR PURPOSE.
 
-import os
 import argparse
+import sys
 from peregrine.samples import load_samples
 from peregrine.acquisition import AcquisitionResult
 from peregrine import defaults
 from peregrine.log import default_logging_config
-import peregrine.tracking as tracking
+from peregrine.tracking import Tracker
 from peregrine.gps_constants import L1CA, L2C
 from peregrine.initSettings import initSettings
+
 
 def main():
   default_logging_config()
 
   parser = argparse.ArgumentParser()
-  parser.add_argument("file",
-                      help="the sample data file to process")
 
-  parser.add_argument("-f", "--file-format",
-                      help="the format of the sample data file "
-                      "('piksi', 'int8', '1bit', '1bitrev', "
-                      "'1bit_x2', '2bits', '2bits_x2', '2bits_x4')")
+  if sys.stdout.isatty():
+    progress_bar_default = 'stdout'
+  elif sys.stderr.isatty():
+    progress_bar_default = 'stderr'
+  else:
+    progress_bar_default = 'none'
 
-  parser.add_argument("-t", "--ms-to-track",
-                      help="the number of milliseconds to track."
-                      "(-1: use all available data",
-                      default="-1")
+  parser.add_argument("--progress-bar",
+                      metavar='FLAG',
+                      choices=['stdout', 'stderr', 'none'],
+                      default=progress_bar_default,
+                      help="Show progress bar. Default is '%s'" %
+                      progress_bar_default)
 
-  parser.add_argument("-s", "--sampling-freq",
-                      help="sampling frequency [Hz]. ")
+  inputCtrl = parser.add_argument_group('Data Source',
+                                        'Data source configuration')
+  inputCtrl.add_argument("file",
+                         help="The sample data file to process")
 
-  parser.add_argument("--profile",
-                      help="L1C/A & L2C IF + sampling frequency profile"
-                      "('peregrine'/'custom_rate', 'low_rate', "
-                      "'normal_rate' (piksi_v3), 'high_rate')",
-                      default='peregrine')
+  inputCtrl.add_argument("-f", "--file-format",
+                         choices=['piksi', 'int8', '1bit', '1bitrev',
+                                  '1bit_x2', '2bits', '2bits_x2', '2bits_x4'],
+                         metavar='FORMAT',
+                         help="The format of the sample data file "
+                         "('piksi', 'int8', '1bit', '1bitrev', "
+                         "'1bit_x2', '2bits', '2bits_x2', '2bits_x4')")
 
-  parser.add_argument("-P", "--prn",
-                      help="PRN to track. ")
+  inputCtrl.add_argument("-t", "--ms-to-track",
+                         metavar='MS',
+                         help="the number of milliseconds to track."
+                         "(-1: use all available data",
+                         default="-1")
 
-  parser.add_argument("-p", "--code-phase",
-                      help="code phase [chips]. ")
+  inputCtrl.add_argument("--skip-samples",
+                         default=0,
+                         metavar='N_SAMPLES',
+                         help="How many samples to skip")
 
-  parser.add_argument("-d", "--carr-doppler",
-                      help="carrier Doppler frequency [Hz]. ")
+  inputCtrl.add_argument("-s",
+                         "--sampling-freq",
+                         metavar='FREQ',
+                         help="Sampling frequency [Hz]. ")
 
-  parser.add_argument("-o", "--output-file", default="track.csv",
-                      help="Track results file name. "
-                      "Default: %s" % "track.csv")
+  inputCtrl.add_argument("--profile",
+                         choices=['peregrine', 'custom_rate', 'low_rate',
+                                  'normal_rate', 'piksi_v3', 'high_rate'],
+                         metavar='PROFILE',
+                         help="L1C/A & L2C IF + sampling frequency profile"
+                         "('peregrine'/'custom_rate', 'low_rate', "
+                         "'normal_rate', 'piksi_v3', 'high_rate')",
+                         default='peregrine')
 
-  parser.add_argument("-S", "--signal",
-                      choices=[L1CA, L2C],
-                      help="Signal type (l1ca / l2c)")
+  signalParam = parser.add_argument_group('Signal tracking',
+                                          'Parameters for satellite vehicle'
+                                          ' signal')
 
-  parser.add_argument("--l2c-handover",
-                      action='store_true',
-                      help="Perform L2C handover",
-                      default=False)
+  signalParam.add_argument("-P", "--prn",
+                           help="PRN to track. ")
 
-  parser.add_argument('--l1ca-profile',
-                      help='L1 C/A stage profile',
-                      choices=defaults.l1ca_stage_profiles.keys())
+  signalParam.add_argument("-p", "--code-phase",
+                           metavar='CHIPS',
+                           help="Code phase [chips]. ")
 
-  parser.add_argument("--pipelining",
-                      type=float,
-                      nargs='?',
-                      help="FPGA pipelining coefficient",
-                      const=defaults.pipelining_k,
-                      default=None)
+  signalParam.add_argument("-d", "--carr-doppler",
+                           metavar='DOPPLER',
+                           help="carrier Doppler frequency [Hz]. ")
 
-  parser.add_argument("--skip-samples", default=0,
-                      help="How many samples to skip")
+  signalParam.add_argument("-S", "--signal",
+                           choices=[L1CA, L2C],
+                           metavar='BAND',
+                           help="Signal type (l1ca / l2c)")
+  signalParam.add_argument("--l2c-handover",
+                           action='store_true',
+                           help="Perform L2C handover",
+                           default=False)
+  signalParam.add_argument('--l1ca-profile',
+                           metavar='PROFILE',
+                           help='L1 C/A stage profile. Controls coherent'
+                                ' integration time and tuning parameters: %s.' %
+                                str(defaults.l1ca_stage_profiles.keys()),
+                           choices=defaults.l1ca_stage_profiles.keys())
+
+  fpgaSim = parser.add_argument_group('FPGA simulation',
+                                      'FPGA delay control simulation')
+  fpgaExcl = fpgaSim.add_mutually_exclusive_group(required=False)
+  fpgaExcl.add_argument("--pipelining",
+                        type=float,
+                        nargs='?',
+                        metavar='PIPELINING_K',
+                        help="Use FPGA pipelining simulation. Supply optional "
+                        " coefficient (%f)" % defaults.pipelining_k,
+                        const=defaults.pipelining_k,
+                        default=None)
+
+  fpgaExcl.add_argument("--short-long-cycles",
+                        type=float,
+                        nargs='?',
+                        metavar='PIPELINING_K',
+                        help="Use FPGA short-long cycle simulation. Supply"
+                        " optional pipelining coefficient (0.)",
+                        const=0.,
+                        default=None)
+
+  outputCtrl = parser.add_argument_group('Output parameters',
+                                         'Parameters that control output'
+                                         ' data stream.')
+  outputCtrl.add_argument("-o", "--output-file",
+                          default="track.csv",
+                          help="Track results file name. Default: %s" %
+                               "track.csv")
 
   args = parser.parse_args()
 
@@ -133,18 +190,22 @@ def main():
   ms_to_track = int(args.ms_to_track)
 
   if args.pipelining is not None:
-    tracker_options = {'mode': 'pipelining', 'k': args.pipelining}
+    tracker_options = {'mode': 'pipelining',
+                       'k': args.pipelining}
+  elif args.short_long_cycles is not None:
+    tracker_options = {'mode': 'short-long-cycles',
+                       'k': args.short_long_cycles}
   else:
     tracker_options = None
 
-  acq_result = AcquisitionResult(prn = prn,
-                    snr = 25, # dB
-                    carr_freq = IF + carr_doppler,
-                    doppler = carr_doppler,
-                    code_phase = code_phase,
-                    status = 'A',
-                    signal = signal,
-                    sample_index = skip_samples)
+  acq_result = AcquisitionResult(prn=prn,
+                                 snr=25,  # dB
+                                 carr_freq=IF + carr_doppler,
+                                 doppler=carr_doppler,
+                                 code_phase=code_phase,
+                                 status='A',
+                                 signal=signal,
+                                 sample_index=skip_samples)
 
   if args.l1ca_profile:
     profile = defaults.l1ca_stage_profiles[args.l1ca_profile]
@@ -155,13 +216,13 @@ def main():
     stage2_params = None
 
   samples = {L1CA: {'IF': freq_profile['GPS_L1_IF']},
-              L2C: {'IF': freq_profile['GPS_L2_IF']},
+             L2C: {'IF': freq_profile['GPS_L2_IF']},
              'samples_total': -1,
              'sample_index': skip_samples}
 
-  samples = load_samples(samples = samples,
-                         filename = args.file,
-                         file_format = args.file_format)
+  load_samples(samples=samples,
+               filename=args.file,
+               file_format=args.file_format)
 
   if ms_to_track < 0:
     # use all available data
@@ -182,15 +243,16 @@ def main():
   print "Tracker options:                        %s" % str(tracker_options)
   print "======================================================================"
 
-  tracker = tracking.Tracker(samples = samples,
-                    channels = [acq_result],
-                    ms_to_track = ms_to_track,
-                    sampling_freq = sampling_freq,  # [Hz]
-                    l2c_handover = l2c_handover,
-                    stage2_coherent_ms = stage2_coherent_ms,
-                    stage2_loop_filter_params = stage2_params,
-                    tracker_options = tracker_options,
-                    output_file = args.output_file)
+  tracker = Tracker(samples=samples,
+                    channels=[acq_result],
+                    ms_to_track=ms_to_track,
+                    sampling_freq=sampling_freq,  # [Hz]
+                    l2c_handover=l2c_handover,
+                    stage2_coherent_ms=stage2_coherent_ms,
+                    stage2_loop_filter_params=stage2_params,
+                    tracker_options=tracker_options,
+                    output_file=args.output_file,
+                    progress_bar_output=args.progress_bar)
   tracker.start()
   condition = True
   while condition:
@@ -199,9 +261,9 @@ def main():
       condition = False
     else:
       samples['sample_index'] = sample_index
-      samples = load_samples(samples = samples,
-                             filename = args.file,
-                             file_format = args.file_format)
+      load_samples(samples=samples,
+                   filename=args.file,
+                   file_format=args.file_format)
   tracker.stop()
 
 if __name__ == '__main__':
