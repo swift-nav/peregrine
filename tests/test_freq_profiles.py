0026cd252cc064d572c79de37adc744061871364
Adel Mamin <adelm@exafore.com>
2016-04-21 18:47:11 +0300

    Disable test for frequency profiles (to be re-enabled later)

--- tests/test_freq_profiles.py
+++ tests/test_freq_profiles.py
@@ -6,11 +6,9 @@ from peregrine.samples import load_samples
 import peregrine.defaults as defaults
 import os
 
-def test_freq_profiles():
-  #run_acq_test(-4000, 0, [1], '2bits', 'custom_rate')
-  run_acq_test(-2000, 0, [2], '2bits', 'low_rate')
-  run_acq_test(2000, 0, [3], '2bits', 'normal_rate')
-  run_acq_test(4000, 0, [4], '2bits', 'high_rate')
-
-if __name__ == '__main__':
-  test_freq_profiles()
+# TODO: enable this
+# def test_freq_profiles():
+#   #run_acq_test(-4000, 0, [1], '2bits', 'custom_rate')
+#   run_acq_test(-2000, 0, [2], '2bits', 'low_rate')
+#   run_acq_test(2000, 0, [3], '2bits', 'normal_rate')
+#   run_acq_test(4000, 0, [4], '2bits', 'high_rate')
