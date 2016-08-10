7563695b997560c143a461540c7d441e2ffd3d4a
Adel Mamin <adelm@exafore.com>
2016-08-10 17:26:22 +0300

    Change PLL BW values order

--- peregrine/defaults.py
+++ peregrine/defaults.py
@@ -207,7 +207,7 @@ l2c_loop_filter_params = {
 
 l1ca_track_params = {
     'fll_bw': (1, 0),
-    'pll_bw': (40, 35, 30, 25, 20, 18, 16, 14, 10, 8, 6, 4, 2),
+    'pll_bw': (40, 35, 30, 25, 20, 18, 16, 14, 12, 10, 8, 7, 6.5, 6, 5.5, 5),
     'coherent_ms': (1, 2, 4, 5, 10, 20) }
 
 l1ca_loop_filter_params_template = {
